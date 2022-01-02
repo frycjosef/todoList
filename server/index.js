07520a33b26fef31f58c0d8ad6819a3660de8719
josef.fryc@tul.cz <josef.fryc@tul.cz>
2022-01-02 17:38:37 +0100

    handle production

--- server/index.js
+++ server/index.js
@@ -44,6 +44,14 @@ const users = require('./routes/api/users');
 app.use('/api/tasks', tasks);
 app.use('/api/users', users);
 
+//Handle production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public'));
+
+    //Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
